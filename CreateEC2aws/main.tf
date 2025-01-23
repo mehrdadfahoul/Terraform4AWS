092717964683e93b56db903609762ba11bb117bd
mehrdadfahoul <mehrdad.fahoul@gmail.com>
2025-01-23 16:22:53 +0330

    Update and Improve the code

--- CreateEC2aws/main.tf
+++ CreateEC2aws/main.tf
@@ -1,29 +1,24 @@
 
 #Create by @mehrdadfahoul
-
 provider "aws" {
   region = var.aws_region
 }
-
 #Create Security Group for access SSH port 22
 resource "aws_security_group" "SG-ssh" {
     name = "allow ssh port"
     description = "Enable ssh port to access your EC2"
-
     ingress {
         from_port = 22
         to_port = 22
-        protocol = tcp
+        protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"] #Don`t forget if you have Public Ip, it is better to use is it here
     }
-
     egress {
         from_port = 0
         to_port = 0
+        protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
-        protocol = tcp
     }
-
     tags = {
         Name = "allow_ssh"
     }
@@ -33,9 +28,7 @@ resource "aws_security_group" "SG-ssh" {
 resource "aws_instance" "my_ec2_instance" {
     ami = var.ami_id #your ami id it is related to your region
     instance_type = var.instance_type #your instance type, for useing FreeTire you should use t2.micro
-
     vpc_security_group_ids = [aws_security_group.SG-ssh.id]  #for attach to your EC2
-
     tags = {
       Name = var.instance_name
       Environment = var.environment
